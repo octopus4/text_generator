f0984a56a6b6c4fbe05b9dec4e5e6954fddc19d3
Gigolaev Artur Valeryevich <a.gigolaev@tinkoff.ru>
2019-12-31 14:55:46 +0300

    Added validation function, exception list as an incoming parameter, fixed argument values retrieving, refactored variable names

--- train.py
+++ train.py
@@ -4,11 +4,19 @@ import pickle
 import preprocessing
 
 
+def validate(arguments):
+    if arguments.input_dir is None:
+        raise ValueError("Empty data directory path is not allowed")
+    if arguments.model is None:
+        raise ValueError("Empty model directory path is not allowed")
+
+
 def read_data(input_directory: str) -> str:
     if input_directory is None:
         return input()
-    with open(input_directory, 'rb') as data:
-        return ' '.join(map(str, data.readlines()))
+    with open(input_directory, 'r', encoding='utf8') as data:
+        lines = data.readlines()
+        return ' '.join(lines)
 
 
 def save_model(model_obj: models.MarkovChain, model_directory: str):
@@ -19,15 +27,18 @@ def save_model(model_obj: models.MarkovChain, model_directory: str):
 parser = argparse.ArgumentParser()
 parser.add_argument("--input-dir", default=None, type=str, help="Directory path to training data file")
 parser.add_argument("--model", default=None, type=str, help="Directory path to save trained model")
+parser.add_argument("--exception-terms", default="", type=str,
+                    help="String with symbols excepted from the ignore list")
 
 args = parser.parse_args()
-input_dir = args["input-dir"]
-model_dir = args["model"]
+validate(args)
+input_dir = args.input_dir
+model_dir = args.model
+exception_terms = args.exception_terms
 
-training_data = read_data(input_dir)
-settings = preprocessing.Settings()
-tokenizer = preprocessing.Tokenizer(settings, training_data)
-tokens = tokenizer.get_tokens()
+raw_data = read_data(input_dir)
+settings = preprocessing.Settings(exceptions=exception_terms)
+training_data = preprocessing.tokenize(raw_data, settings)
 model = models.MarkovChain()
-model.fit(tokens)
+model.fit(training_data)
 save_model(model, model_dir)
