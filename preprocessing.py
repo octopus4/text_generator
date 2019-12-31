2bfc467aed838879347fdab6589d837cb346b12d
Gigolaev Artur Valeryevich <a.gigolaev@tinkoff.ru>
2019-12-31 14:57:38 +0300

    Included regexp

--- preprocessing.py
+++ preprocessing.py
@@ -1,28 +1,26 @@
+import re
 from typing import List
 
 
 class Settings:
-    def __init__(self, ignore_list=".,;()*&^%#@!~`'\"/\\+-<>{}|", exceptions="", to_lower_case=True):
+    def __init__(self, ignore_list=".,;()*&^%#@!?~`'\"/\\+-<>{}|\n", exceptions="", to_lower_case=True):
         self.ignore_list = ignore_list
         self.exceptions = exceptions
         self.to_lower_case = to_lower_case
 
 
-class Tokenizer:
-    @classmethod
-    def fromfilename(cls, settings: Settings, filename: str):
-        return cls(settings, "")
-
-    def __parse_text__(self, text: str) -> List[str]:
-        settings = self.__settings
-        ignore_list = settings.ignore_list
-        exceptions = settings.exceptions
-        to_lower_case = set
-        return []
-
-    def __init__(self, settings: Settings, text: str):
-        self.__settings: Settings = settings
-        self.__tokens: List[str] = self.__parse_text__(text)
-
-    def get_tokens(self):
-        return self.__tokens
+def tokenize(text: str, settings: Settings) -> List[str]:
+    settings = settings
+    ignore_list = settings.ignore_list
+    exceptions = settings.exceptions
+    to_lower_case = settings.to_lower_case
+    if to_lower_case:
+        text = text.lower()
+    ignore_tokens = set(list(ignore_list))
+    exception_tokens = set(list(exceptions))
+    pattern = '[' + ignore_list + ']'
+    text = re.replace()
+    for ignore_token in ignore_tokens:
+        text = text.replace(ignore_token, ' ' + ignore_token)
+    ignore_tokens = ignore_tokens.difference(exception_tokens)
+    return list(filter(None, text.split()))
