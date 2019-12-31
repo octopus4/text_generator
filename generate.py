8f333a45da56bd51f613d1755fd779465d7fd687
Gigolaev Artur Valeryevich <a.gigolaev@tinkoff.ru>
2019-12-31 14:58:24 +0300

    Fixed arg values retrieving

--- generate.py
+++ generate.py
@@ -8,9 +8,9 @@ parser.add_argument("--seed", default=None, type=str, help="Start word of the se
 parser.add_argument("--length", default=None, type=int, help="Maximal length of generated sequence")
 
 args = parser.parse_args()
-model_dir = args["model"]
-seed = args["seed"]
-length = args["length"]
+model_dir = args.model
+seed = args.seed
+length = args.length
 
 with open(model_dir, 'rb') as model_file:
     model_pickle = pickle.load(model_file)
