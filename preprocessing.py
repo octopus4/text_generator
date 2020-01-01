a19efa51f14e38ff49305169c25838029a740e96
Artur Gigolaev <laokrit@gmail.com>
2020-01-01 20:35:02 +0300

    Used regular expressions to segregate delimiters

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,21 @@
 import re
 from typing import List
 
+delimiters = r"([\.\,\;\(\)\*\&\^\%\#\@\!\?\~\`\'\"\/\\\+\-\<\>\{\}\|\n])"
+
 
 class Settings:
-    def __init__(self, ignore_list=".,;()*&^%#@!?~`'\"/\\+-<>{}|\n", exceptions="", to_lower_case=True):
+    def __init__(self, ignore_list="-", to_lower_case=True):
         self.ignore_list = ignore_list
-        self.exceptions = exceptions
         self.to_lower_case = to_lower_case
 
 
 def tokenize(text: str, settings: Settings) -> List[str]:
     settings = settings
     ignore_list = settings.ignore_list
-    exceptions = settings.exceptions
     to_lower_case = settings.to_lower_case
     if to_lower_case:
         text = text.lower()
-    ignore_tokens = set(list(ignore_list))
-    exception_tokens = set(list(exceptions))
-    pattern = '[' + ignore_list + ']'
-    text = re.replace()
-    for ignore_token in ignore_tokens:
-        text = text.replace(ignore_token, ' ' + ignore_token)
-    ignore_tokens = ignore_tokens.difference(exception_tokens)
-    return list(filter(None, text.split()))
+    exceptions = set(ignore_list)
+    text = re.sub(delimiters, r" \1", text)
+    return list(filter(lambda x: x not in exceptions, text.split()))
