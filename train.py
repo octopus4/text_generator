b593860e134645a746abd93b133bd96342e90d06
Artur Gigolaev <laokrit@gmail.com>
2020-01-02 04:26:54 +0300

    Moved validation and text file reading functions to the utils module

--- train.py
+++ train.py
@@ -1,21 +1,23 @@
 import argparse
 import models
 import preprocessing
+import utils
+from pathlib import Path
 
 
-def validate(arguments):
-    if arguments.input_dir is None:
-        raise ValueError("Empty data directory path is not allowed")
-    if arguments.model is None:
-        raise ValueError("Empty model directory path is not allowed")
+def validate_input_dir(arg_value: str):
+    is_none_error_msg = "You have to specify path to the data file in the arguments list"
+    does_not_exist_error_msg = "You have to specify a valid path to the data file"
+    if arg_value is None:
+        raise ValueError(is_none_error_msg)
+    if not Path(arg_value).is_file():
+        raise ValueError(does_not_exist_error_msg)
 
 
-def read_data(input_directory: str) -> str:
-    if input_directory is None:
-        return input()
-    with open(input_directory, 'r', encoding='utf8') as data:
-        lines = data.readlines()
-        return ' '.join(lines)
+def validate_model_path(arg_value: str):
+    error_msg = "You have to specify path to the result model file in the arguments list"
+    if arg_value is None:
+        raise ValueError(error_msg)
 
 
 parser = argparse.ArgumentParser()
@@ -24,13 +26,17 @@ parser.add_argument("--model", default=None, type=str, help="Directory path to s
 parser.add_argument("--ignore", default="-", type=str,
                     help="String with symbols that are not taken into account while parsing the input")
 
+validators = {
+    "input_dir": validate_input_dir,
+    "model": validate_model_path
+}
 args = parser.parse_args()
-validate(args)
+utils.validate(vars(args), validators)
 input_dir = args.input_dir
 model_dir = args.model
 ignore_list = args.ignore
 
-raw_data = read_data(input_dir)
+raw_data = utils.read_string(input_dir)
 settings = preprocessing.Settings(ignore_list)
 training_data = preprocessing.tokenize(raw_data, settings)
 model = models.MarkovChain()
--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
 import re
 from preprocessing import delimiters
+from typing import Callable
+from typing import Dict
 
 
 def format_result(text: str):
     return re.sub(r"[ ]*" + delimiters, r"\1", text)
+
+
+def validate(arguments: Dict[str, str], validators: Dict[str, Callable]) -> None:
+    for arg_name in validators.keys():
+        validator = validators[arg_name]
+        arg_value = arguments[arg_name]
+        validator(arg_value)
+
+
+def read_string(filename: str) -> str:
+    if filename is None:
+        return input()
+    with open(filename, 'r', encoding='utf8') as data:
+        lines = data.readlines()
+        return ' '.join(lines)
